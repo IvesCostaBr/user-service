052692e07f07ba7104aad3527b928f00848e67c9
Ives Costa <ivespauiniam@gmail.com>
2023-11-20 08:11:29 -0500

    ajusutando pipeline

---
+++ src/utils/grpc.py
@@ -0,0 +1,13 @@
+class GrpcClient:
+    def __init__(self, host: str):
+        self.host = host
+        
+    def __start__connection():
+        print("stating connection grpc server")
+
+    def __close__connection():
+        print("closing connection grpc server")
+
+    def send_data(self):
+        self.__start__connection()
+        self.__close__connection()
